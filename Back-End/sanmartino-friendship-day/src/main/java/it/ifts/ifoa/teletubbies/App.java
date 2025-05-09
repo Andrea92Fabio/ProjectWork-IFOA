2aa8fef2caab150600b86b3b90947269c8f29e6c
Loris Catizzone <165474578+loriscatiz@users.noreply.github.com>
2025-05-09 10:12:19 +0200

    basic setup for the App class

--- Back-End/sanmartino-friendship-day/src/main/java/it/ifts/ifoa/teletubbies/App.java
+++ Back-End/sanmartino-friendship-day/src/main/java/it/ifts/ifoa/teletubbies/App.java
@@ -1,7 +1,9 @@
 package it.ifts.ifoa.teletubbies;
 
 
+import it.ifts.ifoa.teletubbies.controller.SubmissionsController;
 import it.ifts.ifoa.teletubbies.repository.UserRepository;
+import it.ifts.ifoa.teletubbies.service.MailService;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -11,30 +13,38 @@ import static spark.Spark.*;
 
 public class App
 {
+    Connection connection;
+    UserRepository userRepository;
+    MailService mailService;
+    SubmissionsController submissionsController;
+
 
     public static void main(String[] args)
     {
+        new App().run();
+    }
+
+
+    public App()
+    {
         port(8080);
 
-        Connection connection;
-
-        //todo: configure connection string
+        //todo: setup connection string
         try
         {
-            connection = DriverManager.getConnection("");
+            this.connection = DriverManager.getConnection("");
         }
         catch (SQLException e)
         {
             throw new RuntimeException(e);
         }
+        this.userRepository = new UserRepository(connection);
+        this.mailService = new MailService();
+        this.submissionsController = new SubmissionsController();
+    }
 
-        UserRepository userRepository = new UserRepository(connection);
-
-
-
-        post("/", (req, res) ->
-        {
-            return "";
-        });
+    private void run()
+    {
+        submissionsController.initSubmissionEndpoint();
     }
 }
--- Back-End/sanmartino-friendship-day/src/main/java/it/ifts/ifoa/teletubbies/controller/SubmissionsController.java
+++ Back-End/sanmartino-friendship-day/src/main/java/it/ifts/ifoa/teletubbies/controller/SubmissionsController.java
@@ -1,5 +1,14 @@
 package it.ifts.ifoa.teletubbies.controller;
 
+import static spark.Spark.*;
+
 public class SubmissionsController
 {
+    public void initSubmissionEndpoint()
+    {
+        post("/", (req, res) ->
+        {
+            return "";
+        });
+    }
 }
