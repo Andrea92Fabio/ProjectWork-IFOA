0875ce49051052e77a47247a6ac788b1b30e6215
Loris Catizzone <loriscatiz@gmail.com>
2025-05-09 09:53:23 +0200

    initialize repo in the main

--- Back-End/sanmartino-friendship-day/src/main/java/it/ifts/ifoa/teletubbies/App.java
+++ Back-End/sanmartino-friendship-day/src/main/java/it/ifts/ifoa/teletubbies/App.java
@@ -1,14 +1,39 @@
 package it.ifts.ifoa.teletubbies;
 
 
+import it.ifts.ifoa.teletubbies.repository.UserRepository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import static spark.Spark.*;
 
 public class App
 {
+
     public static void main(String[] args)
     {
         port(8080);
-        post("/", (req, res) -> {
+
+        Connection connection;
+
+        //todo: configure connection string
+        try
+        {
+            connection = DriverManager.getConnection("");
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+
+        UserRepository userRepository = new UserRepository(connection);
+
+
+
+        post("/", (req, res) ->
+        {
             return "";
         });
     }
