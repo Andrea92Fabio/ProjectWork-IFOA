ef515b19c96c523e011180bb468d0d9685d8ab29
Loris Catizzone <loriscatiz@gmail.com>
2025-05-28 10:46:13 +0200

    more animations

--- Front-End/assets/js/home.js
+++ Front-End/assets/js/home.js
@@ -11,15 +11,23 @@ export default function home() {
       });
     },
     {
-      rootMargin: "-40% 0px",
+      rootMargin: "0px 0px -40% 0px",
     }
   );
 
   const cards = document.querySelectorAll(".card");
+
   const fadeInLeft = document.querySelectorAll(".fade-in-left");
   const fadeInTop = document.querySelectorAll(".fade-in-top");
   const fadeInBottom = document.querySelectorAll(".fade-in-bottom");
   const fadeInRight = document.querySelectorAll(".fade-in-right");
+
+  const animatableOnEntranceObjs = [
+    ...fadeInLeft,
+    ...fadeInTop,
+    ...fadeInBottom,
+    ...fadeInRight,
+  ];
 
   cards.forEach((c) => observer.observe(c));
   animatableOnEntranceObjs.forEach((el) => observer.observe(el));
