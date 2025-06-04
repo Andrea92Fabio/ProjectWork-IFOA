4208b4dab0a8482f678c5bfc6c6d202a467665c4
xhydex99 <v.nzo.vitale@gmail.com>
2025-06-04 17:22:14 +0200

    Added mailSender method

--- Back-End/sanmartino-friendship-day/src/main/java/it/ifts/ifoa/teletubbies/service/MailService.java
+++ Back-End/sanmartino-friendship-day/src/main/java/it/ifts/ifoa/teletubbies/service/MailService.java
@@ -1,5 +1,44 @@
 package it.ifts.ifoa.teletubbies.service;
 
-public class MailService
-{
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
+import java.util.Properties;
+
+public class MailService {
+
+    public static void sendEmail(String receiver) {
+        final String senderEmail = "vincenzovitale.ariosto@gmail.com";
+        final String password = "hfyh gabq bcnp tkov";
+
+        String host = "smtp.gmail.com";
+        int port = 587;
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", port);
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAduthentication(){
+            return new PasswordAuthentication(senderEmail, password);
+           }
+        });
+
+        try{
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(senderEmail));
+            message.addRecipient(Message.RecipientType.TO,new InternetAddress(receiver));
+            message.setSubject("Oggetto della mail");
+            message.setText("Corpo della mail");
+            Transport.send(message);
+        } catch (MessagingException e){
+            e.printStackTrace();
+        }
+
+    }
+
 }
