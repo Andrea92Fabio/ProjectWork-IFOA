ea92e37ae5633f416a23c0e2c41ed4271be6d4c7
Loris Catizzone <loriscatiz@gmail.com>
2025-06-09 16:24:20 +0200

    form basic validation

--- Front-End/assets/js/age-check.js
+++ Front-End/assets/js/age-check.js
@@ -1,51 +1,51 @@
-import home, { observer } from "./home.js";
+import home, { observer } from './home.js';
 
 export default function ageCheck() {
-  const view = document.querySelector("#view-age-check");
+    const view = document.querySelector('#view-age-check');
 
-  const animatableOnEntranceObjs = view.querySelectorAll(".fade-in-top");
+    const animatableOnEntranceObjs = view.querySelectorAll('.fade-in-top');
 
-  animatableOnEntranceObjs.forEach((el) => observer.observe(el));
+    animatableOnEntranceObjs.forEach((el) => observer.observe(el));
 
-  let birthdate = sessionStorage.getItem("birthdate");
-  if (birthdate != null && isDrinkingAge(new Date(birthdate))) {
-    home();
-    return;
-  }
-  view.classList.add("active");
-  const form = document.querySelector("#age-check-form");
-  const birthdateInput = document.querySelector("#age-check-birthdate");
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const birthdateObj = new Date(birthdateInput.value);
-    sessionStorage.setItem("birthdate", birthdateObj.toISOString());
-    if (isDrinkingAge(birthdateObj)) {
-      view.classList.remove("active");
-      home();
-    } else {
-      window.location.replace("https://www.beviresponsabile.it/");
+    let birthdate = sessionStorage.getItem('birthdate');
+    if (birthdate != null && isDrinkingAge(new Date(birthdate))) {
+        home();
+        return;
     }
-  });
+    view.classList.add('active');
+    const form = document.querySelector('#age-check-form');
+    const birthdateInput = document.querySelector('#age-check-birthdate');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const birthdateObj = new Date(birthdateInput.value);
+        sessionStorage.setItem('birthdate', birthdateObj.toISOString());
+        if (isDrinkingAge(birthdateObj)) {
+            view.classList.remove('active');
+            home();
+        } else {
+            window.location.replace('https://www.beviresponsabile.it/');
+        }
+    });
 }
 
-function isDrinkingAge(birthdate) {
-  const today = new Date();
+export function isDrinkingAge(birthdate) {
+    const today = new Date();
 
-  if (
-    today.getFullYear() - birthdate.getFullYear() > 18 &&
-    today.getFullYear() - birthdate.getFullYear() < 100
-  ) {
-    return true;
-  } else if (
-    today.getFullYear() - birthdate.getFullYear() < 18 ||
-    today.getFullYear() - birthdate.getFullYear() >= 100
-  ) {
-    return false;
-  } else if (today.getMonth() < birthdate.getMonth()) {
-    return false;
-  } else if (today.getMonth() > birthdate.getMonth()) {
-    return true;
-  }
+    if (
+        today.getFullYear() - birthdate.getFullYear() > 18 &&
+        today.getFullYear() - birthdate.getFullYear() < 100
+    ) {
+        return true;
+    } else if (
+        today.getFullYear() - birthdate.getFullYear() < 18 ||
+        today.getFullYear() - birthdate.getFullYear() >= 100
+    ) {
+        return false;
+    } else if (today.getMonth() < birthdate.getMonth()) {
+        return false;
+    } else if (today.getMonth() > birthdate.getMonth()) {
+        return true;
+    }
 
-  return today.getDate() >= birthdate.getDate();
+    return today.getDate() >= birthdate.getDate();
 }
--- Front-End/assets/js/form.js
+++ Front-End/assets/js/form.js
@@ -1,27 +1,78 @@
-import { observer } from "./home.js";
+import { observer } from './home.js';
+import { isDrinkingAge } from './age-check.js';
 
 export default function form() {
-  const view = document.querySelector("#view-form");
-  view.classList.add("active");
-  view.removeAttribute("aria-hidden");
-  view.removeAttribute("inert");
+    const view = document.querySelector('#view-form');
+    view.classList.add('active');
+    view.removeAttribute('aria-hidden');
+    view.removeAttribute('inert');
 
-  const inputFields = view.querySelectorAll(".input-field");
+    const form = view.querySelector('#main-form');
 
-  inputFields[0].value = sessionStorage.getItem("birthdate").split("T")[0];
+    const inputFields = form.querySelectorAll('.input-field');
 
-  //todo: implement check on blur for input fields (maybe a function with a switch for the type of input)
+    view.querySelector('#form-birthdate').value = sessionStorage
+        .getItem('birthdate')
+        .split('T')[0];
 
-  //todo: implement an extra check on submit
+    inputFields.forEach((el) => {
+        errors[el.id] = '';
+    });
+    //todo: implement an extra check on submit
 
-  //todo: todo: send data as json
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-  const animatableOnEntranceObjs = [
-    ...view.querySelectorAll(".fade-in-left"),
-    ...view.querySelectorAll(".fade-in-top"),
-    ...view.querySelectorAll(".fade-in-right"),
-    ...view.querySelectorAll(".fade-in-bottom"),
-  ];
+        const bdayObj = new Date(form.querySelector('#form-birthdate'));
 
-  animatableOnEntranceObjs.forEach((el) => observer.observe(el));
+        console.log(validateForm());
+    });
+
+    function validateForm() {
+        const errors = new Map();
+
+        errors.set('form-birthdate', null);
+        errors.set('form-name', null);
+
+        const bdayObj = new Date(form.querySelector('#form-birthdate').value);
+
+        if (!isDrinkingAge(bdayObj)) {
+            errors.set(
+                'form-birthdate',
+                'Devi essere maggiorenne per partecipare'
+            );
+        }
+
+        //cap 5 figures
+        //
+
+        errors.forEach((el, key) => {
+            if (el) {
+                form.querySelector(`#${key}-errors`).textContent = el;
+                form.querySelector(`#${key}-wrapper`).classList.remove(
+                    'success'
+                );
+                form.querySelector(`#${key}-wrapper`).classList.add('error');
+            } else {
+                form.querySelector(`#${key}-errors`).textContent = '';
+                form.querySelector(`#${key}-wrapper`).classList.remove('error');
+                form.querySelector(`#${key}-wrapper`).classList.add('success');
+            }
+        });
+
+        console.log(errors);
+
+        return errors.size == 0;
+    }
+
+    //todo: todo: send data as json
+
+    const animatableOnEntranceObjs = [
+        ...view.querySelectorAll('.fade-in-left'),
+        ...view.querySelectorAll('.fade-in-top'),
+        ...view.querySelectorAll('.fade-in-right'),
+        ...view.querySelectorAll('.fade-in-bottom'),
+    ];
+
+    animatableOnEntranceObjs.forEach((el) => observer.observe(el));
 }
