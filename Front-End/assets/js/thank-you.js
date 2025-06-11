43ce1eccfff0cc1a571120197cc7292c1a0e3ac8
Loris Catizzone <loriscatiz@gmail.com>
2025-06-11 10:06:37 +0200

    add thank you view

--- Front-End/assets/js/thank-you.js
+++ Front-End/assets/js/thank-you.js
@@ -1,7 +1,15 @@
+import { name, email } from './form.js';
+
 const view = document.querySelector('#view-thank-you');
 
 export default function thankYou() {
     view.classList.add('active');
     view.removeAttribute('aria-hidden');
     view.removeAttribute('inert');
+
+    const thankYouName = document.querySelector('#thank-you-name');
+    const thankYouEmail = document.querySelector('#thank-you-email');
+
+    thankYouName.textContent = name.value;
+    thankYouEmail.textContent = email.value;
 }
