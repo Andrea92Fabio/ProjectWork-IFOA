255f1380cf51862e9c0812a6be6034babfcd4235
Loris Catizzone <loriscatiz@gmail.com>
2025-06-16 10:44:32 +0200

    something

--- Front-End/assets/js/result.js
+++ Front-End/assets/js/result.js
@@ -4,7 +4,7 @@ getResultFromServer(tokenId);
 
 async function getResultFromServer(tokenId) {
     const response = await fetch(
-        `http://192.168.100.8:80/api/confirmation/${tokenId}`
+        `http://192.168.100.30:80/api/confirmation/${tokenId}`
     );
 
     const json = await response.json();
