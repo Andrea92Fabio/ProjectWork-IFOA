3b9cc344835feddb210a320267c164c9a8eb3481
xhydex99 <v.nzo.vitale@gmail.com>
2025-06-16 09:33:43 +0200

    added checkUser method

--- Back-End/sanmartino-friendship-day/src/main/java/it/ifts/ifoa/teletubbies/entity/User.java
+++ Back-End/sanmartino-friendship-day/src/main/java/it/ifts/ifoa/teletubbies/entity/User.java
@@ -9,31 +9,31 @@ import java.util.Random;
 public class User
 {
 
-    public User() {
-    }
-
     private String name,surname, gender,phoneNumber, tokenId, email, fiscalCode,residencyCountry,residencyZipCode, residencyAddress, shipCountry, shipZipCode, shipAddress;
     private boolean privacy, rules;
     private LocalDate birthDate;
 
-    public User(String name, String surname, String email, String gender, String fiscalCode, String residencyCountry, String residencyZipCode, String residencyAddress, String shipCountry, String shipZipCode, String shipAddress, boolean privacy, boolean rules, String birthDate) throws InvalidNameException, InvalidSurnameException, InvalidGenderException, InvalidPhoneNumberException, InvalidFiscalCodeException, InvalidResidenceCountryException, InvalidZipCodeException, InvalidAddressException, InvalidShipCountryException, InvalidPrivacyException, InvalidRulesException, InvalidBirthDateException {
 
-        this.name = checkName(name);
-        this.surname = checkSurname(surname);
-        this.gender = checkGender(gender);
-        this.email = checkEmail(email);
-        this.residencyCountry = checkResidencyCountry(residencyCountry);
-        this.fiscalCode = checkFiscalCode(fiscalCode);
-        this.residencyZipCode = checkResidencyZipCode(residencyZipCode);
-        this.residencyAddress = checkResidencyAddress(residencyAddress);
-        this.shipCountry = checkShipCountry(shipCountry);
-        this.shipZipCode = checkShipZipCode(shipZipCode);
-        this.shipAddress = checkShipAddress(shipAddress);
-        this.privacy = checkPrivacy(privacy);
-        this.rules = checkRules(rules);
-        this.birthDate = checkBirthDate(birthDate);
-        this.phoneNumber = checkPhoneNumber(phoneNumber);
-//        this.tokenId = assignTokenId();
+    public User() {
+    }
+
+    public void checkUser(){
+        checkEmail(this.email);
+        checkName(this.name);
+        checkSurname(this.surname);
+        checkGender(this.gender);
+        checkPhoneNumber(this.phoneNumber);
+        checkFiscalCode(this.fiscalCode);
+        checkResidencyCountry(this.residencyCountry);
+        checkResidencyAddress(this.residencyAddress);
+        checkResidencyZipCode(this.residencyZipCode);
+        checkShipCountry(this.shipCountry);
+        checkShipAddress(this.shipAddress);
+        checkShipZipCode(this.shipZipCode);
+        checkPrivacy(this.privacy);
+        checkRules(this.rules);
+        checkBirthDate(this.birthDate);
+
     }
 
     public void assignTokenId(){
@@ -51,36 +51,32 @@ public class User
         this.tokenId = retvalue.toString();
     }
 
-    private String checkEmail(String email) throws  InvalidEmailException{
+    private void checkEmail(String email) throws  InvalidEmailException{
         String regex = "^[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z]{2,}&";
         if (!Pattern.matches(regex, email)){
             throw new InvalidEmailException("0x00");
         }
-        return email;
     }
 
-    private String checkName(String name) throws InvalidNameException{
+    private void checkName(String name) throws InvalidNameException{
         if (name == null || name.length() < 2){
             throw new InvalidNameException("0x01");
         }
-        return name;
     }
 
-    private String checkSurname(String surname) throws InvalidSurnameException{
+    private void checkSurname(String surname) throws InvalidSurnameException{
         if (surname == null || surname.length() < 2){
             throw new InvalidSurnameException("0x02");
         }
-        return surname;
     }
 
-    private String checkGender(String gender) throws InvalidGenderException {
+    private void checkGender(String gender) throws InvalidGenderException {
         if(!gender.equals("Uomo") && !gender.equals("Donna") && !gender.equals("ALtro")){
             throw new InvalidGenderException("0x03");
         }
-        return gender;
     }
 
-    private String checkPhoneNumber(String phoneNumber) throws InvalidPhoneNumberException {
+    private void checkPhoneNumber(String phoneNumber) throws InvalidPhoneNumberException {
         if (phoneNumber == null || phoneNumber.trim().isEmpty()) {
             throw new InvalidPhoneNumberException("0x04");
         }
@@ -88,95 +84,82 @@ public class User
         if(!Pattern.matches(regex,phoneNumber)){
             throw new InvalidPhoneNumberException("0x05");
         }
-        return phoneNumber;
     }
 
-    private String checkFiscalCode(String fiscalCode) throws InvalidFiscalCodeException {
+    private void checkFiscalCode(String fiscalCode) throws InvalidFiscalCodeException {
         if(this.residencyCountry.equals("San Marino")){
-            return "";
+            return;
         }
         String regex = "^[a-zA-Z]{6}[a-zA-Z0-9]{2}[a-zA-Z][a-zA-Z0-9]{2}[a-zA-Z][a-zA-Z0-9]{3}[a-zA-Z]+$";
         if ( fiscalCode.length() != 17 && !Pattern.matches(regex,fiscalCode)) {
             throw new InvalidFiscalCodeException("0x06");
         }
-        return fiscalCode;
     }
 
-    private String checkResidencyCountry(String residencyCountry) throws InvalidResidenceCountryException {
+    private void checkResidencyCountry(String residencyCountry) throws InvalidResidenceCountryException {
         if (residencyCountry == null || (!residencyCountry.equals("Italia") && !residencyCountry.equals("San Marino"))) {
             throw new InvalidResidenceCountryException("0x07");
         }
-        return residencyCountry;
     }
 
-    private String checkResidencyZipCode(String residencyZipCode) throws InvalidZipCodeException {
+    private void checkResidencyZipCode(String residencyZipCode) throws InvalidZipCodeException {
         String regex = "^[0-9]{5}";
         if (!Pattern.matches(regex, residencyZipCode) ) {
             throw new InvalidZipCodeException("0x08");
         }
 
-        return residencyZipCode;
     }
 
-    private String checkResidencyAddress(String residencyAddress) throws InvalidAddressException
+    private void checkResidencyAddress(String residencyAddress) throws InvalidAddressException
     {
         if (residencyAddress == null && residencyAddress.length()<=2) {
             throw new InvalidAddressException("0x09");
         }
-        return residencyAddress;
     }
 
 
-    private String checkShipCountry(String shipCountry) throws InvalidResidenceCountryException {
+    private void checkShipCountry(String shipCountry) throws InvalidResidenceCountryException {
         if (shipCountry == null || (!shipCountry.equals("Italia") && !shipCountry.equals("San Marino"))) {
             throw new InvalidResidenceCountryException("0x10");
         }
-        return shipCountry;
     }
 
-    private String checkShipZipCode(String shipZipCode) throws InvalidZipCodeException {
+    private void checkShipZipCode(String shipZipCode) throws InvalidZipCodeException {
         String regex = "^[0-9]{5}";
         if (!Pattern.matches(regex, shipZipCode) ) {
             throw new InvalidZipCodeException("0x11");
         }
-
-        return shipZipCode;
     }
 
-    private String checkShipAddress(String shipAddress) throws InvalidAddressException
+    private void checkShipAddress(String shipAddress) throws InvalidAddressException
     {
         if (shipAddress == null && shipAddress.length()<=2) {
             throw new InvalidAddressException("0x12");
         }
-        return shipAddress;
     }
 
-    private boolean checkPrivacy(boolean privacy) throws InvalidPrivacyException {
+    private void checkPrivacy(boolean privacy) throws InvalidPrivacyException {
         if (!privacy) {
             throw new InvalidPrivacyException("0x13");
         }
-        return privacy;
     }
 
-    private boolean checkRules(boolean rules) throws InvalidRulesException {
+    private void checkRules(boolean rules) throws InvalidRulesException {
         if (!rules) {
             throw new InvalidRulesException("0x14");
         }
-        return rules;
     }
 
-    private LocalDate checkBirthDate(String birthDate) throws InvalidBirthDateException {
+    private void checkBirthDate(LocalDate birthDate) throws InvalidBirthDateException {
         if (birthDate == null) {
             throw new InvalidBirthDateException("0x15");
         }
-        LocalDate retvalue = LocalDate.parse(birthDate);
         LocalDate now = LocalDate.now();
         LocalDate minimumAge = now.minusYears(18);
 
-        if (retvalue.isAfter(minimumAge)) {
+        if (birthDate.isAfter(minimumAge)) {
             throw new InvalidBirthDateException("0x16");
         }
-        return retvalue;
     }
 
     public String getName() {
