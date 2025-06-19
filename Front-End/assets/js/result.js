28eb31f70878b099ee45b2e9312e660ee82c53dd
Loris Catizzone <loriscatiz@gmail.com>
2025-06-19 16:41:51 +0200

    fix result bug

--- Front-End/assets/js/result.js
+++ Front-End/assets/js/result.js
@@ -3,18 +3,22 @@ const tokenId = params.get('tokenId');
 const messageTarget = document.querySelector('#result-message');
 const wrapper = document.querySelector('#result-wrapper');
 
-displayResult(getResultFromServer(tokenId));
+displayResult(await getResultFromServer(tokenId));
 
 async function getResultFromServer(tokenId) {
     const response = await fetch(
         `http://192.168.100.45:80/api/confirmation/${tokenId}`
     );
 
-    return await response.json();
+    const retvalue = await response.json();
+    console.log(retvalue.message);
+    return retvalue;
 }
 
 function displayResult(result) {
-    if (result === 'you win') {
+    console.log(result.message);
+
+    if (result.message == 'you win') {
         messageTarget.textContent = 'Complimenti! Hai vinto';
         wrapper.classList.add('win');
     } else {
